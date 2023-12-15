6bb726cf7e63b1778cf71de3f3771138b3c5212a
Vakarė Lazickaitė <vakarei.l@gmail.com>
2023-12-15 15:08:36 +0200

    Defined classes for the game

---
+++ war.py
@@ -0,0 +1,48 @@
+import random
+import sys
+
+class Card():
+    def __init__():
+        ...
+    ...
+
+class Deck:
+    def __init__():
+        ...
+    ...
+
+class Player:
+    def __init__():
+        ...
+
+class Game:
+    def __init__():
+        ...
+
+
+# option 1
+deck_of_cards = [
+    'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK', 'HA',
+    'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK', 'DA',
+    'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK', 'CA',
+    'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK', 'SA'
+]
+
+# option 2
+deck_of_cards = [
+    Card('H', '2'), Card('H', '3'), Card('H', '4'), Card('H', '5'), Card('H', '6'),
+    Card('H', '7'), Card('H', '8'), Card('H', '9'), Card('H', '10'), Card('H', 'J'),
+    Card('H', 'Q'), Card('H', 'K'), Card('H', 'A'),
+
+    Card('D', '2'), Card('D', '3'), Card('D', '4'), Card('D', '5'), Card('D', '6'),
+    Card('D', '7'), Card('D', '8'), Card('D', '9'), Card('D', '10'), Card('D', 'J'),
+    Card('D', 'Q'), Card('D', 'K'), Card('D', 'A'),
+
+    Card('C', '2'), Card('C', '3'), Card('C', '4'), Card('C', '5'), Card('C', '6'),
+    Card('C', '7'), Card('C', '8'), Card('C', '9'), Card('C', '10'), Card('C', 'J'),
+    Card('C', 'Q'), Card('C', 'K'), Card('C', 'A'),
+
+    Card('S', '2'), Card('S', '3'), Card('S', '4'), Card('S', '5'), Card('S', '6'),
+    Card('S', '7'), Card('S', '8'), Card('S', '9'), Card('S', '10'), Card('S', 'J'),
+    Card('S', 'Q'), Card('S', 'K'), Card('S', 'A')
+]
